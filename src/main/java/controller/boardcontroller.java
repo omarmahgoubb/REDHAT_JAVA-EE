84e8f79158149064ba1fcfe067f42feae2d61e82
ahmedaymanawwaad <167222106+ahmedaymanawwaad@users.noreply.github.com>
2024-05-04 03:07:56 +0300

    2.a trial_1

--- src/main/java/controller/boardcontroller.java
+++ src/main/java/controller/boardcontroller.java
@@ -1,27 +1,27 @@
-//package controller;
-//
-//import model.Board;
-//import service.Boardservice;
-//
-//import javax.ejb.EJB;
-//import javax.ejb.Stateful;
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
-//import java.util.List;
-//
-//@Stateful
-//@Path("boards")
-//@Produces(MediaType.APPLICATION_JSON)
-//@Consumes(MediaType.APPLICATION_JSON)
-//
-//public class boardcontroller {
-//	@EJB
-//    private Boardservice boardservice;
-//	@POST
-//	@Path("create")
-//	public String createboard(Board board) {
-//		return boardservice.createBoard(board); 
-//	}
-//	
-//
-//}
+package controller;
+
+import model.Board;
+import service.Boardservice;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Stateful
+@Path("boards")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
+public class boardcontroller {
+	@EJB
+    private Boardservice boardservice;
+	@POST
+	@Path("create")
+	public String createboard(Board board) {
+		return boardservice.createBoard(board); 
+	}
+	
+
+}
